2766e23e179eceac54a8981292c56c2d184d0c8d
supal <supal.iut03@gmail.com>
2019-10-12 14:17:40 +0600

    the first commit

---
+++ src/main/java/co/getmehired/social/GetMeSocialApplication.java
@@ -0,0 +1,13 @@
+package co.getmehired.social;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GetMeSocialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GetMeSocialApplication.class, args);
+	}
+
+}
