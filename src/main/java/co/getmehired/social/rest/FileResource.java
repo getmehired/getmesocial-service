69a70fa1a46953a09b200ffae068507d6dbd8d73
supal <supal.iut03@gmail.com>
2019-12-06 08:31:53 +0600

    Update FileResource.java

--- src/main/java/co/getmehired/social/rest/FileResource.java
+++ src/main/java/co/getmehired/social/rest/FileResource.java
@@ -18,6 +18,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/api/files")
 public class FileResource {
